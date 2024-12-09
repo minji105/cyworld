8be6ccc03f17c5ded0f9aef1048279b0a8aa26f1
minji105 <ajhjmj9@naver.com>
2024-12-09 18:21:42 +0900

    meta

--- pages/_app.js
+++ pages/_app.js
@@ -9,8 +9,11 @@ function App({ Component, pageProps }) {
     <>
       <Head>
         <meta name="title" content="조민지님의 미니홈피" />
+        <meta name="application-name" content="조민지님의 미니홈피"></meta>
+        <meta name="author" content="조민지"></meta>
         <meta name="description" content="싸이월드를 재현하여 만든 미니홈피입니다." />
         <meta name="keywords" content="미니홈피, 싸이월드" />
+        <meta name="google-site-verification" content="YT2ed87ec-uxvMV765Y78BYLFEs1CKVUe7xi8VH6JVw" />
         <meta property="og:title" content="조민지님의 미니홈피" />
         <meta property="og:description" content="싸이월드를 재현하여 만든 미니홈피입니다." />
         
