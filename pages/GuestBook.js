c315ae648fb1add3be2d5f4db0a96087ee5d4b36
minji105 <ajhjmj9@naver.com>
2024-12-12 19:20:00 +0900

    update GuestBook api

--- pages/GuestBook.js
+++ pages/GuestBook.js
@@ -47,7 +47,7 @@ function GuestBook() {
       date: new Date().toLocaleDateString(),
     };
 
-    axios.post('http://localhost:3001/api/entries', newEntry)
+    axios.post(`${process.env.NEXT_PUBLIC_API_UR}/api/entries`, newEntry)
       .then(response => {
         setEntries([newEntry, ...entries]);
         setName("");
