5506d54926732168a60867bc98a5dc7dac8548a7
minji105 <ajhjmj9@naver.com>
2024-12-16 02:12:58 +0900

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,13 @@ import './global/Nes.css';
 import Head from 'next/head';
 import Diary from '../src/component/diary/Diary';
 import Side from '../src/component/side/Side';
+import { Html } from 'next/document';
 
 function App({ Component, pageProps }) {
   return (
-    <>
+    <Html lang='ko'>
       <Head>
+        <meta charset="UTF-8"></meta>
         <meta name="title" content="조민지님의 미니홈피" />
         <meta name="application-name" content="조민지님의 미니홈피"></meta>
         <meta name="author" content="조민지"></meta>
@@ -16,13 +18,12 @@ function App({ Component, pageProps }) {
         <meta name="google-site-verification" content="YT2ed87ec-uxvMV765Y78BYLFEs1CKVUe7xi8VH6JVw" />
         <meta property="og:title" content="조민지님의 미니홈피" />
         <meta property="og:description" content="싸이월드를 재현하여 만든 미니홈피입니다." />
-        <meta charset="UTF-8"></meta>
         <title>조민지님의 미니홈피</title>
       </Head>
       <Diary />
       <Side />
       <Component {...pageProps} />
-    </>
+    </Html>
   );
 }
 
